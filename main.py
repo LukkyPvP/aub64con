7ce21c193e8cf386e441edcdaa0085a1e2ffa24f
Luki <87608187+LukkyPvP@users.noreply.github.com>
2025-02-03 18:05:25 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -15,11 +15,17 @@ def decoding_unicode(num, decoding='utf-8'):
     except UnicodeDecodeError:
         return f"Ошибка: символ {num} не найден в {decoding}"
 
+def decoding_base65(num, decoding='utf-8'):
+    try:
+        return base64.b64decode(num).decode(decoding)
+    except UnicodeDecodeError:
+        return f"Ошибка: символ {num} не найден в {decoding}"
+
 def coding_ascii(num, coding='utf-8'):
     try:
         return ord(num.encode(coding))
-    except UnicodeDecodeError:
-        return f"Ошибка: символ {str} не найден в {coding}"
+    except UnicodeEncodeError:
+        return f"Ошибка: строка {str} не смогла закодироваться с помощью {coding}"
 
 def coding_unicode(num, coding='utf-8'):
     try:
@@ -27,12 +33,18 @@ def coding_unicode(num, coding='utf-8'):
             return ord(num)
         else:
             return num.decode().encode(f'{coding}')
-    except UnicodeDecodeError:
-        return f"Ошибка: символ {num} не найден в {coding}"
+    except UnicodeEncodeError:
+        return f"Ошибка: строка {num} не смогла закодироваться с помощью {coding}"
 
-selectcodingordecoding = input("coding or decoding?: ")
+def coding_base65(num, coding='utf-8'):
+    try:
+        return base64.b64encode(num).encode(coding)
+    except UnicodeDecodeError:
+        return f"Ошибка: строка {num} не смогла закодироваться с помощью {coding}"
+
+selectcodingordecoding = input("coding или decoding?: ")
 if selectcodingordecoding == "decoding":
-    selectdecoding = input("ASCII Type, Unicode or Base65?: ")
+    selectdecoding = input("ASCII Type, Unicode или Base65?: ")
     if selectdecoding == "ASCII":
         shar = int(input("Введите символ: "))
         decoding = input("Введите кодировку: ")
@@ -43,7 +55,8 @@ if selectcodingordecoding == "decoding":
         print(decoding_unicode(shar, decoding))
     elif selectdecoding == "Base65":
         base65str = input("Введите Base65 строку: ")
-        print(base64.b64decode(base65str).decode('windows-1251'))
+        base65decoding = input("Введите кодировку: ")
+        print(decoding_base65(base65str, base65decoding))
 elif selectcodingordecoding == "coding":
     selectcoding = input("ASCII Type, Unicode or Base65?: ")
     if selectcoding == "ASCII":
@@ -56,4 +69,5 @@ elif selectcodingordecoding == "coding":
         print(coding_unicode(shar, coding))
     elif selectcoding == "Base65":
         base65str = input("Введите строку: ")
-        print(base64.b64encode(base65str.encode('windows-1251')))
+        base65coding = input("Введите кодировку: ")
+        print(coding_base65(base65str, base65coding))
